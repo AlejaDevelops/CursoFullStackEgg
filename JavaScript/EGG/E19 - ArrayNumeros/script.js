5e4f379f83fa8133a441001511851ad26b3ab710
Alejandra Orjuela <orjuela.alejandrap@gmail.com>
2023-08-23 16:06:53 -0500

    21th commit

--- JavaScript/EGG/E19 - ArrayNumeros/script.js
+++ JavaScript/EGG/E19 - ArrayNumeros/script.js
@@ -1,13 +1,40 @@
-const a = [];
-const b = [];
+const arrayA = [];
+const arrayB = [];
+
+let longitud1 = 50;
+let longitud2 = 20;
 
 //Crear vector 50 numeros
-function llenarArray(a) {}
-for (let i = 0; i < 50; i++) {
-  a[i] = (Math.random() * 100).toFixed(1);
+function llenarArray(arrayA) {}
+for (let i = 0; i < longitud1; i++) {
+  let n = Math.floor(Math.random() * 100); //Acerca n al número entrero inferior
+
+  arrayA.push(n);
 }
 
-console.log(a);
+console.log("Array A aleatorio " + arrayA);
+alert("Array A aleatorio " + arrayA);
+
+function compareNumeric(n, m) {
+  if (n > m) return 1;
+  if (n == m) return 0;
+  if (n < m) return -1;
+}
+
+arrayA.sort(compareNumeric);
+
+for (let i = 0; i < longitud2; i++) {
+  if (i < 10) {
+    arrayB.push(arrayA[i]);
+  } else {
+    arrayB.push(0.5);
+  }
+}
+
+console.log(`Array A ordenado ${arrayA} 
+Array B ${arrayB}`);
+alert(`Array A ordenado ${arrayA} 
+Array B ${arrayB}`);
 
 /* SOLUCION MARIO
 
---
+++ JavaScript/EGG/E20 - ArrayEliminarAgregarElemento/script.js
@@ -0,0 +1,8 @@
+let matriz1 = [[3], [6], [9], [12], [15]];
+
+//delete matriz1[0];
+
+matriz1.splice(0, 5, 6, 9, 12, 15, 18);
+
+alert(matriz1);
+console.log(matriz1);
